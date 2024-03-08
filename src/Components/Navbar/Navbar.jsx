66bec163c2c0cbcd28e1482d03eca680bdc79eb5
mansi patil <patilmansi1201@gmail.com>
2024-03-08 22:50:35 +0530

    navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../Assets/logo.png'
+
+export const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className="logo">
+            <img src={logo} alt="logo" style={{width: '100px', height: 'auto'}} />
+            <p className="logo-text">PETWELL</p>
+        </div>
+        <ul className="nav-menu">
+            <li>Home <hr/></li>
+            <li>Community</li>
+        </ul>
+        <div className="nav-login">
+            <button className="login-button">Login</button>
+        </div>
+
+
+    </div>
+  )
+}
+
+export default Navbar
