6da06dbb9b47041a2ec9227038bd7680d8f6534a
mansi patil <patilmansi1201@gmail.com>
2024-04-09 18:06:47 +0530

    bg

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
-import './Hero.css'
-import arrow_icon from '../Assets/arrow.png'
-import hero_image from '../Assets/hero_image.png'
+import React from 'react';
+import './Hero.css';
+import arrow_icon from '../Assets/arrow.png';
 
 const Hero = () => {
   return (
@@ -9,23 +8,19 @@ const Hero = () => {
       <div className="hero-left">
         <h2>WELCOME TO PETWELL</h2>
         <div>
-            <div className="hero-hand-icon">
-                <p>WHERE</p>
-            </div>
-            <p>WELLNESS</p>
-            <p>BEGINS....</p>
+          <div className="hero-hand-icon">
+            <p>WHERE</p>
+          </div>
+          <p>WELLNESS</p>
+          <p>BEGINS....</p>
         </div>
         <div className="hero-latest-btn">
-            <div>Chat with us</div>
-            
-            <img src={arrow_icon} alt="" />
+          <div>Chat with us</div>
+          <img src={arrow_icon} alt="" />
         </div>
       </div>
-      <div className="hero-right">
-        <img src={hero_image} alt="" />
-      </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
