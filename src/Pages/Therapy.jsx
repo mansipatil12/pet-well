87de1082dad749c7b39ed8489afc688ff303bf29
mansi patil <patilmansi1201@gmail.com>
2024-04-17 23:50:20 +0530

    update - features and therapy page

--- src/Pages/Therapy.jsx
+++ src/Pages/Therapy.jsx
@@ -5,17 +5,17 @@ import 'D:/pet-well/src/Pages/CSS/Therapy.css';
 const Therapy = () => {
     return (
         <div className="therapy-container">
-            <div className="therapy-box">
+            <div className="therapy-box-1">
                 <h2>Video Consultation</h2>
                 <p>Book a video consultation with our therapists for personalized sessions.</p>
                 <button>Book Now</button>
             </div>
-            <div className="therapy-box">
+            <div className="therapy-box-2">
                 <h2>Connect with Animal Shelters</h2>
                 <p>Explore our network of animal shelters to find your perfect therapy companion.</p>
                 <button>Find Shelters</button>
             </div>
-            <div className="therapy-box" id="idk-box">
+            <div className="therapy-box-3">
                 <h2>I Don't remember</h2>
                 <p>I forgot what 3rd box was supposed to be:/</p>
                 <button>Explore</button>
