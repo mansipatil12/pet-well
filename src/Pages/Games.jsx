0f1a61ba9df63c5bb79e44c7e06a51394b8b1e0a
mansi patil <patilmansi1201@gmail.com>
2024-04-19 14:19:25 +0530

    game url update

--- src/Pages/Games.jsx
+++ src/Pages/Games.jsx
@@ -5,18 +5,18 @@ import 'D:/pet-well/src/Pages/CSS/Games.css';
 const Games = () => {
     // Define an array of game objects with image URLs
     const games = [
-        { id: 1, imageUrl: 'game1.jpg' },
-        { id: 2, imageUrl: 'game2.jpg' },
-        { id: 3, imageUrl: 'game3.jpg' },
-        { id: 4, imageUrl: 'game4.jpg' },
-        { id: 5, imageUrl: 'game5.jpg' },
-        { id: 6, imageUrl: 'game6.jpg' },
-        { id: 7, imageUrl: 'game7.jpg' },
-        { id: 8, imageUrl: 'game8.jpg' },
-        { id: 9, imageUrl: 'game9.jpg' },
-        { id: 10, imageUrl: 'game10.jpg' },
-        { id: 11, imageUrl: 'game11.jpg' },
-        { id: 12, imageUrl: 'game12.jpg' },
+        { id: 1, imageUrl: 'game1.jpg', gameUrl: 'https://example.com/game1' },
+        { id: 2, imageUrl: 'game2.jpg', gameUrl: 'https://example.com/game1' },
+        { id: 3, imageUrl: 'game3.jpg', gameUrl: 'https://example.com/game1' },
+        { id: 4, imageUrl: 'game4.jpg', gameUrl: 'https://example.com/game1' },
+        { id: 5, imageUrl: 'game5.jpg', gameUrl: 'https://example.com/game1' },
+        { id: 6, imageUrl: 'game6.jpg', gameUrl: 'https://example.com/game1' },
+        { id: 7, imageUrl: 'game7.jpg', gameUrl: 'https://example.com/game1' },
+        { id: 8, imageUrl: 'game8.jpg', gameUrl: 'https://www.crazygames.com/game/funny-food-duel' },
+        { id: 9, imageUrl: 'game9.jpg', gameUrl: 'https://www.santagames.net/games/cat-and-mouse.htm' },
+        { id: 10, imageUrl: 'game10.jpg', gameUrl: 'https://poki.com/en/g/catpad' },
+        { id: 11, imageUrl: 'game11.jpg', gameUrl: 'https://poki.com/en/g/kitty-cats' },
+        { id: 12, imageUrl: 'game12.jpg', gameUrl: 'https://poki.com/en/g/elixpur-idle'},
     ];
 
     return (
